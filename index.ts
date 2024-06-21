14fde8f2cdf7a581ae3aa0770ad2cc30be0885b5
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-06-21 11:04:24 +0200

    update session schedule

--- index.ts
+++ index.ts
@@ -1,27 +1,12 @@
-import createHandover from "./src/createHandover";
 import createMarkdownTable from "./src/createMardownTable";
 import parseSchedule from "./src/parseSchedule";
 
-const input = `
-JS Functions
-JS Functions 2
-HTML Forms 🆕
-JS Inputs and Strings 🆕 / Active Learning
-JS Objects and Arrays
-JS Forms 🆕
-JS createElement
-📒 Self Directed Learning - Week 3
-Recap Project 2
-Recap Project 2
-`;
+const file = Bun.file("./input.txt");
+const input = await file.text();
+
 const rows = parseSchedule(input);
 
 const markdownTable = createMarkdownTable(rows);
-const handover = createHandover(rows);
+// const handover = createHandover(rows);
 
-const output = Bun.file("./output.md").writer();
-output.flush();
-output.write(markdownTable);
-output.write(`\n---\n`);
-output.write(handover);
-output.flush();
+Bun.write("./output.md", markdownTable);
--- src/createMardownTable.ts
+++ src/createMardownTable.ts
@@ -1,26 +1,56 @@
-export default function createMarkdownTable(rows: string[]): string {
-  const dayData: { [day: string]: { am: string; pm: string } } = {
-    Monday: { am: "", pm: "" },
-    Tuesday: { am: "", pm: "" },
-    Wednesday: { am: "", pm: "" },
-    Thursday: { am: "", pm: "" },
-    Friday: { am: "", pm: "" },
-  };
-  const days = Object.keys(dayData);
+function createFullTimeMarkdownTable(rows: string[]): string {
+	const dayData: { [day: string]: { am: string; pm: string } } = {
+		Monday: { am: "", pm: "" },
+		Tuesday: { am: "", pm: "" },
+		Wednesday: { am: "", pm: "" },
+		Thursday: { am: "", pm: "" },
+		Friday: { am: "", pm: "" },
+	};
+	const days = Object.keys(dayData);
 
-  rows.forEach((session, index) => {
-    const dayIndex = Math.floor(index / 2);
-    if (dayIndex >= days.length) return;
+	rows.forEach((session, index) => {
+		const dayIndex = Math.floor(index / 2);
+		if (dayIndex >= days.length) return;
 
-    dayData[days[dayIndex]][index % 2 === 0 ? "am" : "pm"] = session;
-  });
+		dayData[days[dayIndex]][index % 2 === 0 ? "am" : "pm"] = session;
+	});
 
-  const table = [
-    "| Day | am | pm |\n| --- | -- | -- |",
-    ...days.map(
-      (day) => `| ${day} | ${dayData[day].am} | ${dayData[day].pm} |`
-    ),
-  ].join("\n");
+	const table = [
+		"| Day | am | pm |",
+		"| --- | --- | --- |",
+		...days.map(
+			(day) => `| ${day} | ${dayData[day].am} | ${dayData[day].pm} |`,
+		),
+	].join("\n");
 
-  return table;
+	return table;
 }
+
+function createPartTimeMarkdownTable(rows: string[]): string {
+	const dayData: { [day: string]: string | null } = {
+		Monday: null,
+		Tuesday: null,
+		Wednesday: null,
+		Thursday: null,
+		Friday: null,
+	};
+	const days = Object.keys(dayData);
+
+	rows.forEach((session, dayIndex) => {
+		if (dayIndex >= days.length) return;
+
+		dayData[days[dayIndex]] = session;
+	});
+
+	const table = [
+		"| Day | Session |",
+		"| --- | --- |",
+		...days.map((day) => `| ${day} | ${dayData[day]} |`),
+	].join("\n");
+
+	return table;
+}
+
+export default process.env.COHORT_TYPE === "full-time"
+	? createFullTimeMarkdownTable
+	: createPartTimeMarkdownTable;
